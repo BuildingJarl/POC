cf0983eb0bb23c2188c5ac19c75ec5b9f173eec9
BuildingJarl <BuildingJarl@users.noreply.github.com>
2015-05-29 09:55:49 +0100

    cleaned server.js

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ var bodyParser = require('koa-body-parser');
 var koa = require('koa');
 var app = koa();
 
-
 app.use( common.logger('dev') );
 app.use( favicon( __dirname + '/public/favicon.ico' ) );
 
@@ -26,33 +25,11 @@ publicRouter.get('/', function*(next) {
 	this.body = yield this.render('index');
 });
 
-publicRouter.post('/givemeyourdata', function*(next) {
-	//console.log(this.request.header)
-	this.status = 204;
-
-	writeFile(this.request.body);
-});
-
 app.use( bodyParser( { jsonLimit: 10 } ) );
 app.use( publicRouter.middleware() );
-
 
 // listen
 var port = process.env.PORT || 9000;
 var server = app.listen(port, function () {
     console.log('Listening on port %d', server.address().port);
 });
-
-
-function writeFile( data ) {
-	
-	//var obj = JSON.parse( data );
-
-	fs.writeFile( "./results/" + data.uuid + ".json", JSON.stringify(data, null, '\t'), function(err) {
-	    if(err) {
-	        console.log(err);
-	    } else {
-	        console.log("The file " + data.uuid +" was saved!");
-	    }
-	});
-}
