42da4acf4bc6183df62440a548a3b74349abd9d9
BuildingJarl <BuildingJarl@users.noreply.github.com>
2015-12-09 10:23:28 +0000

    save

--- public/src/main.js
+++ public/src/main.js
@@ -1,6 +1,7 @@
 import esprima from 'esprima';
 import * as escope from 'escope';
-import THREE from 'three.js';
+//import THREE from 'three.js';
+//import 'three.js/examples/js/renderers/CSS3DRenderer';
 import ace from 'ace';
 
 //Editor CLASS Object
@@ -12,7 +13,7 @@ class Editor {
 
 		this.aceEditor.setTheme("ace/theme/monokai");
 		this.aceEditor.getSession().setMode("ace/mode/javascript");
-		this.aceEditor.setValue('');
+		this.aceEditor.setValue('Hello');
 	}
 }
 
@@ -55,69 +56,74 @@ class Vis {
 	}
 }
 
-class ScopeObjectFactory {
-	createCircle(r, color) {
-		var material = new THREE.MeshBasicMaterial({
-			color: color
-		});
+class CssR {
+	constructor(element) {
+		this.element = element;
 
-		var radius = r;
-		var segments = 64;
+		let w = element.clientWidth;
+		let h = element.clientHeight;
 
-		var circleGeometry = new THREE.CircleGeometry( radius, segments );				
-		var circle = new THREE.Mesh( circleGeometry, material );
+		this.renderer = new THREE.CSS3DRenderer();
+		this.renderer.setSize( w, h );
+
+		this.element.appendChild( this.renderer.domElement );
+
+		//camera
 		
-		return circle;
+		let viewSize = h;
+		let aspectRatio = w/h; //1 unit should be one pixel
+		let left = (-aspectRatio * viewSize) / 2;
+		let right = (aspectRatio * viewSize) / 2;
+		let top = viewSize / 2;
+		let bottom = -viewSize / 2;
+		let near = 1;
+		let far = 100;
+
+		this.camera = new THREE.OrthographicCamera( left, right, top, bottom, near, far );
+		this.camera.position.set(0,0,50)
+		
+
+		//this.camera = new THREE.PerspectiveCamera(50, w / h, 1, 10000);
+		//this.camera.position.z = 900;
+		
+		this.scene = new THREE.Scene();
+		this.scene.add( this.camera );
+	}
+
+	add(obj) {
+		this.scene.add( obj );
+	}
+
+	draw() {
+		this.renderer.render( this.scene, this.camera );
 	}
 }
 
+//var vis = new Vis( document.getElementById('viewport1') );
+var cssr = new CssR( document.getElementById('viewport2'));
 
-class ScopeNode extends THREE.Mesh {
-	constructor( r, color ) {
+var geometry = new THREE.BoxGeometry( 20, 20, 20 );
+var material = new THREE.MeshBasicMaterial({color:0xffff00});
+var mesh = new THREE.Mesh( geometry, material );
 
-		var material = new THREE.MeshBasicMaterial({
-			color: color
-		});
+//vis.add( mesh );
+//vis.draw();
 
-		var radius = r;
-		var segments = 64;
-
-		var circleGeometry = new THREE.CircleGeometry( radius, segments );				
-
-		super(circleGeometry, material)
-	}
-}
-
-class ScopeTree {
-	constructor(){
-		this.root = new ScopeNode( 400, 0x000000 );
-	}
-}
-
-var editor = new Editor( document.getElementById('editorPanel') );
-var vis = new Vis( document.getElementById('visPanel') );
-
-var scopeTree = new ScopeTree();
-
-vis.add( scopeTree.root );
-vis.draw();
+var number = document.createElement( 'div' );
+	number.className = 'number';
+	number.textContent = "THREE.JS";
 
 
-/*
-var esprimaOptions = {
-			loc: true,
-			range: true,
-			tolerant: true
-		};
+var editorDiv = document.createElement( 'div' );
+editorDiv.className = 'editor';
+var editor = new Editor(editorDiv);
 
-		var content = this.editor.getSession().getValue();
-		var ast;
-		var error;
+var object = new THREE.CSS3DObject( editorDiv );
+object.position.set( cssr.element.clientWidth/2, cssr.element.clientHeight/2, 0);
+//object.position.set( cssr.element.clientWidth/2, cssr.element.clientHeight/2, 0);
+//object.rotation.set(mesh.rotation.x,mesh.rotation.y,0)
 
-		try {
-			ast = esprima.parse( content , esprimaOptions );
-		}
-		catch(err) {
-			error = err;
-		}
-*/
+cssr.add(object);
+cssr.draw();
+
+console.log(object.rotation)
